10722c5acb29af2c60dfe46e914fd6da0fee0118
jonandermorenoo88 <jaincera97@gmail.com>
2024-06-21 16:34:34 +0200

    obtengo el idviaje el precio y el numero de plazas libres despues tambien creo una excepcion si no hay viajes

--- comprarBillete.sql
+++ comprarBillete.sql
@@ -1,10 +1,29 @@
-create or replace procedure comprarBillete( arg_hora recorridos.horaSalida%type,
-          arg_fecha viajes.fecha%type, arg_origen recorridos.estacionorigen%type,
-          arg_destino recorridos.estaciondestino%type, arg_nroPlazas viajes.nplazaslibres%type
+create or replace procedure comprarBillete( 
+    arg_hora recorridos.horaSalida%type,
+    arg_fecha viajes.fecha%type, 
+    arg_origen recorridos.estacionorigen%type,
+    arg_destino recorridos.estaciondestino%type, 
+    arg_nroPlazas viajes.nplazaslibres%type
 ) is 
+    v_idViaje viajes.idViaje%type;
+    v_precio recorridos.precio%type;
+    v_numPlazasLibres viajes.nPlazasLibres%type;
+    c_noViaje EXCEPTION;
 
 begin
-
-  null;
+  begin
+        select v.idViaje, r.precio, v.nPlazasLibres
+        into v_idViaje, v_precio, v_numPlazasLibres
+        from viajes v
+        join recorridos r on v.idRecorrido = r.idRecorrido
+        where r.estacionOrigen = arg_origen
+          and r.estacionDestino = arg_destino
+          and to_char(r.horaSalida, 'HH24:MI:SS') = to_char(arg_hora, 'HH24:MI:SS')
+          and v.fecha = arg_fecha
+        for update of v.nPlazasLibres;
+    exception
+        when no_data_found then
+            RAISE c_noViaje;
+    end;
   
 end comprarBillete;
